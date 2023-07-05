a3c1e37b802a227e0547e7ff79cfee8a0d597c27
MrEmmanuel <sejesoo@gmail.com>
2023-07-05 20:24:31 +0200

    initial commit of all files

---
+++ src/test/java/com/eviro/assessment/grad001/OreneileSejeso/OreneileSejesoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eviro.assessment.grad001.OreneileSejeso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OreneileSejesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
